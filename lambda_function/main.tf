c1076837cf2fe25acb94d41da2dc3cbbb1239d46
Lua Govinda Mendes Souza <govinda.souza@mercadolivre.com>
2024-03-29 11:25:23 -0300

    up

--- lambda_function/main.tf
+++ lambda_function/main.tf
@@ -1,3 +1,9 @@
 resource "aws_lambda_function" "lambda" {
-  // Configuração da função Lambda
+    function_name = "MinhaFuncaoLambda"
+    handler      = "lambda_function.lambda_handler"
+    runtime      = "python3.8"
+    filename     = "lambda_function.zip"
+    role         = aws_iam_role.lambda_exec.arn
+    timeout      = 10
+    memory_size  = 128
 }
